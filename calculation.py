cbd930ecb7842b50ec894cac1d923f9c6bd53f59
BuildTools <unconfigured@null.spigotmc.org>
2023-03-19 10:21:56 +0300

    Calculation 0.2

--- calculation.py
+++ calculation.py
@@ -5,11 +5,17 @@ Reactor_Power = 0  # 1 fuel = 1%
 Engine_Power = 0  # W = max 80%
 Electr_Power = 0
 Days = 0
-Mass = 1
-Oxygen = 0
-Temp = 0
-Gen = 0
+Mass_Const = 192  # M without SH
+Oxygen = 0  # Oxi
+Temp = 0  # T e [0;30]°C
+Gen = 0  # G
 
-k_Growth = sin(radians(-pi / 2 + pi * (Temp + 0.5 * Oxygen)))
-Electr_Points = Electr_Power * Days * 11
-Gen += Gen * k_Growth
+
+# Calculations
+k_Growth = sin(degrees(-pi / 2 + pi * (Temp + 0.5 * Oxygen)))
+Electr_Points = Electr_Power * Days * 11 #
+Gen += Gen * k_Growth # G
+if Gen < 8:
+    Gen = 0
+Mass = Mass_Const + Gen # M
+#Energy_Temp  # E(T)
