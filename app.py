d1a836be3269589cae9fac72d9799fa33fe2da65
rubicus <laba625@yandex.ru>
2023-03-19 12:35:24 +0300

    FIll

--- app.py
+++ app.py
@@ -16,10 +16,12 @@ Window.size = (1280, 720)
 class Container(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.filled = False
 
         self.data_tables = MDDataTable(
             pos_hint={"center_y": 0.5, "center_x": 0.5},
             size_hint=(0.9, 0.6),
+            padding=10,
             use_pagination=False,
             column_data=[
                 ("День/Точка", dp(30)),
@@ -29,27 +31,38 @@ class Container(BoxLayout):
                 ("Характеристика автоклава", dp(40)),
                 ("Популяция SH", dp(40)),
             ],
-            row_data=[(f"{i + 1}", "0", "0", "0", "0", "0") for i in range(1)],
+            row_data=[("0", "0", "0", "0", "0", "0") for i in range(0)],
         )
         self.add_widget(self.data_tables)
-        self.ids["label"].text = f"Количество дней: {4}"
+        self.ids["label"].text = f"Количество дней: {0}"
+
+    def update_days(self, days):
+        self.ids["label"].text = f"Количество дней: {days}"
 
     def fill_rows(self, data: List[Day]):
         c = 0
         for i in data:
             c += 1
-            d = [str(i.day_num), str(f"Топливо {i.remain_fuel} / Кислород {i.remain_oxi}"),
+            d = (str(i.day_num), str(f"Топливо {i.remain_fuel} / Кислород {i.remain_oxi}"),
                  str(f"Топливо {i.spend_fuel} / Кислород {i.spend_fuel}"),
                  f"{i.engine_percent} / {i.electr_percent}",
-                 str(i.autoclav_temp), str(i.sh)]
+                 str(i.autoclav_temp), str(i.sh))
 
-            self.data_tables.update_row(
-                self.data_tables.row_data[c],  # old row data
-                data,  # new row data
-            )
+            if self.filled:
+                self.data_tables.update_row(
+                    self.data_tables.row_data[c],
+                    d,  # new row data
+                )
+            else:
+                self.data_tables.add_row(
+                    d
+                )
 
     def get_task(self):
-        print('afsdf')
+        data=[Day(),Day(),Day(),Day(),Day()]
+        days=0
+        self.fill_rows(data)
+        self.update_days(days)
 
 
 class App(MDApp):
